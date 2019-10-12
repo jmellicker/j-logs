7112deb46fb30fac1c722ad2d87276386eaff590
jmellicker <jmellicker@users.noreply.github.com>
2019-10-11 19:33:50 -0700

    added magenta, brightened colors for dev tools dark mode

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ nodeColors = {
     g: (str) => { nodePrint('#31D80C', 'hex', str) },
     y: (str) => { nodePrint('#E4D600', 'hex', str) },
     o: (str) => { nodePrint('#E86400', 'hex', str) },
+    m: (str) => { nodePrint('#f533b7', 'hex', str) },
     r: (str) => { nodePrint('#F00000', 'hex', str) },
 
     ph: (str) => { nodePrint('#C16DFF', 'hex', makeHeader(str)) },
@@ -19,6 +20,7 @@ nodeColors = {
     gh: (str) => { nodePrint('#31D80C', 'hex', makeHeader(str)) },
     yh: (str) => { nodePrint('#E4D600', 'hex', makeHeader(str)) },
     oh: (str) => { nodePrint('#E86400', 'hex', makeHeader(str)) },
+    mh: (str) => { nodePrint('#f533b7', 'hex', makeHeader(str)) },
     rh: (str) => { nodePrint('#F00000', 'hex', makeHeader(str)) },
 
     pb: (str) => { nodePrint('#5E00A0', 'bgHex', str) },
@@ -27,6 +29,7 @@ nodeColors = {
     gb: (str) => { nodePrint('#177F00', 'bgHex', str) },
     yb: (str) => { nodePrint('#A39800', 'bgHex', str) },
     ob: (str) => { nodePrint('#E86400', 'bgHex', str) },
+    mb: (str) => { nodePrint('#f533b7', 'bgHex', str) },
     rb: (str) => { nodePrint('#F00000', 'bgHex', str) },
 
     pbh: (str) => { nodePrint('#5E00A0', 'bgHex', makeHeader(str)) },
@@ -35,22 +38,23 @@ nodeColors = {
     gbh: (str) => { nodePrint('#177F00', 'bgHex', makeHeader(str)) },
     ybh: (str) => { nodePrint('#A39800', 'bgHex', makeHeader(str)) },
     obh: (str) => { nodePrint('#E86400', 'bgHex', makeHeader(str)) },
+    mbh: (str) => { nodePrint('#f533b7', 'bgHex', makeHeader(str)) },
     rbh: (str) => { nodePrint('#F00000', 'bgHex', makeHeader(str)) }
 }
 
 function nodePrint(color, type, str) {
-    if (typeof str === 'object') str = JSON.stringify(str, null, 2)
-    console.log(chalk[type](color)(str))
+    console.log(chalk[type](color)(pretty(str)))
 }
 
 browserColors = {
 
-    p(str) { browserPrint(str, 'color: #8a2be2;') },
-    b(str) { browserPrint(str, 'color: blue;') },
+    p(str) { browserPrint(str, 'color: #a875d9;') },
+    b(str) { browserPrint(str, 'color: #6485cc;') },
     c(str) { browserPrint(str, 'color: #00bbcc;') },
     g(str) { browserPrint(str, 'color: green;') },
     y(str) { browserPrint(str, 'color: #ffcc00;') },
     o(str) { browserPrint(str, 'color: #ffa500;') },
+    m(str) { browserPrint(str, 'color: #f533b7;') },
     r(str) { browserPrint(str, 'color: red;') },
 
     ph(str) { this.p(makeHeader(str)); },
@@ -59,6 +63,7 @@ browserColors = {
     gh(str) { this.g(makeHeader(str)); },
     yh(str) { this.y(makeHeader(str)); },
     oh(str) { this.o(makeHeader(str)); },
+    mh(str) { this.m(makeHeader(str)); },
     rh(str) { this.r(makeHeader(str)); },
 
     pb(str) { browserPrint(str, 'background: #8a2be2; color: white; padding: 3px;') },
@@ -67,6 +72,7 @@ browserColors = {
     gb(str) { browserPrint(str, 'background: green; color: white; padding: 3px;') },
     yb(str) { browserPrint(str, 'background: #ffcc00; color: black; padding: 3px;') },
     ob(str) { browserPrint(str, 'background: #ffa500; color: white; padding: 3px;') },
+    mb(str) { browserPrint(str, 'background: #f533b7; color: white; padding: 3px;') },
     rb(str) { browserPrint(str, 'background: red; color: white; padding: 3px;') },
 
     pbh(str) { this.pb(makeHeader(str)); },
@@ -75,15 +81,20 @@ browserColors = {
     gbh(str) { this.gb(makeHeader(str)); },
     ybh(str) { this.yb(makeHeader(str)); },
     obh(str) { this.ob(makeHeader(str)); },
+    mbh(str) { this.mb(makeHeader(str)); },
     rbh(str) { this.rb(makeHeader(str)); }
 
 }
 
 function browserPrint(str, css) {
-    if (typeof str === 'object') str = JSON.stringify(str, null, 2)
-    console.log('%c ' + str + ' ', css)
+    console.log('%c ' + pretty(str) + ' ', css)
 }
 
-function makeHeader(str) { return '======== ' + str + ' ========' }
+function pretty(str) {
+    if (typeof str === 'object') str = JSON.stringify(str, null, 2)
+    return str
+}
+
+function makeHeader(str) { return '======== ' + pretty(str) + ' ========' }
 
 module.exports = isBrowser ? browserColors : nodeColors
